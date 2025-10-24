db7c344772b7c16db73774d91a7fb29ec7e24222
Ngô Thanh Phúc <ntphuc2202@gmail.com>
2025-10-24 20:28:17 +0700

    tokenizer v1.0

--- stage_1/data_preparation_and_sampling/tokenizer.py
+++ stage_1/data_preparation_and_sampling/tokenizer.py
@@ -3,11 +3,13 @@ import re
 class TokenizerV1:
     def __init__(self,vocab):
         self.string2idtoken = vocab
+        self.string2idtoken.update({"<|unk|>":len(self.string2idtoken)+1,"<|endoftext|>":len(self.string2idtoken)+1})
         self.idstoken2string = {v:k for k,v in vocab.items()}
 
     def encode(self,text):
-        tokens = re.split(r'([{,.:;_-+=~!@#$%^&*<>?|\s+}])', text)
-        ids_token = [self.string2idtoken[tokens[i]] for i in range(len(tokens))]
+        tokens = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        tokens = [item.strip() for item in tokens if item.strip()]
+        ids_token = [self.string2idtoken[tokens[i]] if tokens[i] in self.string2idtoken else self.string2idtoken["<|unk|>"] for i in range(len(tokens))]
         return ids_token
     
     def decode(self,ids_token):
@@ -16,7 +18,15 @@ class TokenizerV1:
     
 
 if __name__ == "__main__":
-    with open('vocab.txt','r') as f:
+    with open("../../data/the-verdict.txt",'r') as f:
         text = f.read()
-    
-    
+    words_set = list(set(re.split(r'([,.:;?_!"()\']|--|\s)', text)))
+    words_set.sort()
+    words_set = [item.strip() for item in words_set if item.strip()]
+    vocab = {words_set[i]:i for i in range(len(words_set))}
+    tokenizer = TokenizerV1(vocab)
+    sample_text = text[:10]
+    encoded = tokenizer.encode(sample_text)
+    print("Sample text: ", sample_text)    
+    print("Encoded: ", encoded)
+    print("Decoded: ", tokenizer.decode(encoded))
